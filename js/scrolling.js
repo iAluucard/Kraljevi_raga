4df933d904d1c2d7223e893ff5b95ecb21405d35
iAluucard <44082899+iAluucard@users.noreply.github.com>
2019-10-02 15:08:51 +0200

    :)

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,13 +1,10 @@
-/*$(document).scroll(function () {
+/*$(document).ready(function(){
+    $(document).scroll(function () {
     var scroll = $(window).scrollTop();
-    $("video").css("top", "0" + (scroll / 0.8) + "px");
-});*/
+    $("background-image").css("top", "0" + (scroll / 0.8) + "px");
+});});*/
+
+console.log(document.images);
 
 
-jQuery( document ).ready( function( $ ) {
-	var body = document.body,
-        doc = document.documentElement;
-    $(window).scroll(function () {
-        body.style.backgroundPosition = "0px " + ( 830 -(Math.max(doc.scrollTop, body.scrollTop) / 4) ) + "px";
-    });
-});
+
